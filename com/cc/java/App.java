2067c7b7844f1bd35d981a8e27c35abab3948512
MichaelErichMueller <Michael.Erich.Mueller@gmx.de>
2022-11-23 19:01:33 +0100

    Update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,10 +1,31 @@
 package com.cc.java;
 
 public class App {
+    static int Wert =100000;
     
+
     public static void main(String[] args) {
-     
-    }
+
+    
+Konto konto1 = new Konto(Wert);
+Konto konto2 = new Konto(Wert);
+Konto konto3 = new Konto(Wert);
+
+output(String.valueOf(konto1.getKontostand()));
+output(String.valueOf(konto2.getKontostand()));
+output(String.valueOf(konto3.getKontostand()));
+
+konto1.setKontostand(Wert*2);
+output(String.valueOf(konto1.getKontostand()));
+
+konto2.setKontostand(Wert*3);
+output(String.valueOf(konto2.getKontostand()));
+
+konto3.setKontostand(Wert*10);
+output(String.valueOf(konto3.getKontostand()));
+}
+
+
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
---
+++ com/cc/java/Konto.java
@@ -0,0 +1,26 @@
+package com.cc.java;
+
+public class Konto {
+
+
+private int kontostand;
+
+    public int Wert;
+    public Konto(int kontostand) {
+    this.kontostand = kontostand;
+}
+
+
+    public int getKontostand() {
+        return kontostand;
+    }
+
+
+    public void setKontostand(int kontostand) {
+        this.kontostand = kontostand;
+    }
+
+
+
+}
+
